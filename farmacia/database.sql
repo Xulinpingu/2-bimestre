317fa54f502efededa2184ebb7bae6e2f425115d
Henrique <henriquebigo01@gmail.com>
2026-05-22 12:31:16 -0300

    funcao de pesquisa

--- farmacia/database.sql
+++ farmacia/database.sql
@@ -1,33 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 14/05/2026 às 21:34
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+CREATE DATABASE IF NOT EXISTS farmacia_db;
+
+USE farmacia_db;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `farmacia_db`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `produtos`
---
-
-CREATE TABLE `produtos` (
+CREATE TABLE IF NOT EXISTS `produtos` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `fabricante` varchar(100) NOT NULL,
@@ -35,27 +15,9 @@ CREATE TABLE `produtos` (
   `estoque` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `produtos`
---
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `produtos`
---
 ALTER TABLE `produtos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- farmacia/index.php
+++ farmacia/index.php
@@ -1,3 +1,21 @@
+<?php
+$urlAtual = $_SERVER['REQUEST_URI'];
+$partesUrl = parse_url($urlAtual);
+
+$search_placeholder = "";
+
+if(isset($partesUrl['query'])) {
+    $tag_url_partes = explode("=", $partesUrl['query']);
+    $tag_url = $tag_url_partes[0];
+    $search_placeholder = $tag_url;
+}
+else{
+    header("Location: " . $_SERVER['REQUEST_URI'] . "?nome=");
+    $search_placeholder = "nome";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -19,22 +37,30 @@
     />
 </head>
 <body>
-    <?php include "includes/header.php" ?>
+    <?php require_once "includes/header.php" ?>
     
     <div class="search-bar">
         <i class="ph ph-magnifying-glass"></i>
-        <input type="text">
-        <button><i class="ph ph-tag"></i></button>
+        <input type="text" placeholder="<?= $search_placeholder; ?>" id="search-input">
+        <div class="dropdown">
+            <button class="dropdownbtn"><i class="ph ph-tag"></i></button>
+            <div class="dropdown-content">
+                <button onclick="SearchByTag('id')">ID</button>
+                <button onclick="SearchByTag('nome')">Nome</button>
+                <button onclick="SearchByTag('fabricante')">Fabricante</button>
+                <button onclick="SearchByTag('preco')">Preço</button>
+                <button onclick="SearchByTag('estoque')">Estoque</button>
+            </div>
+        </div>
     </div>
 
     <div class="content">
         <div class="action-btns">
             <button onclick="window.location.href='cadastro.php'" class="btn">Adicionar<i class="ph ph-plus-circle"></i></button>
-            <button onclick="window.location.href='editar.php'" class="btn">Editar <i class="ph ph-note-pencil"></i></button>
         </div>
 
         <?php 
-            if (isset($_GET['id'])) {
+            if (isset($_GET['id']) && isset($_GET['action']) && $_GET['action'] === 'delete') {
                 $id_produto = $_GET['id'];
                 include "excluir.php";
             }
@@ -60,10 +86,17 @@
                         <?php
                             include "config/conexao.php";
 
-                            $sql = "SELECT * FROM produtos ORDER BY id ASC";
-                            $stmt = $pdo->prepare($sql);
-                            $stmt->execute();
+                            if (isset($partesUrl['query'])) {
+                                $tag_url_final = end($tag_url_partes);
+                                $sql = "SELECT * FROM produtos WHERE $tag_url LIKE :valor ORDER BY id ASC";
+                                $stmt = $pdo->prepare($sql);
+                                $stmt->bindValue(':valor', "%$tag_url_final%", PDO::PARAM_STR);
+                            } else {
+                                $sql = "SELECT * FROM produtos ORDER BY id ASC";
+                                $stmt = $pdo->prepare($sql);
+                            }
 
+                            $stmt->execute();
                             $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                             if ($produtos) {
@@ -75,10 +108,10 @@
                                     echo "<td>R$ " . number_format($produto['preco'], 2, ',', '.') . "</td>";
                                     echo "<td>" . $produto['estoque'] . "</td>";
                                     echo "<td><button class='delete-icon' onclick=\"showDelete(" . $produto['id'] . ")\"><i class='ph ph-trash'></i></button></td>";
+                                    echo "<td><button class='edit-icon' onclick=\"window.location.href='editar.php'\"><i class='ph ph-note-pencil'></i></button></td>";
                                     echo "</tr>";
                                 }
-                            }
-                            else {
+                            } else {
                                 echo "<tr class='tbody'><td colspan='6'>Nenhum produto encontrado.</td></tr>";
                             }
                         ?>
@@ -90,12 +123,41 @@
     </div>
 
     <script>
-        function showDelete(id) {
-            window.location.href = '?id=' + id;
+        const search_input = document.getElementById('search-input');
+
+        // Preencher o campo de pesquisa com o valor atual da URL
+        const urlParams = new URLSearchParams(window.location.search);
+        const query = urlParams.get('query');
+        if (query) {
+            search_input.value = decodeURIComponent(query);
         }
+
+        // Adicionar evento para realizar a pesquisa apenas quando o valor mudar
+        let lastSearchValue = search_input.value; // Armazena o último valor pesquisado
+        search_input.addEventListener('input', function(event) {
+            const valorAtual = event.target.value;
+
+            // Só realiza a pesquisa se o valor for alterado
+            if (valorAtual !== lastSearchValue) {
+                lastSearchValue = valorAtual;
+                const url = new URL(window.location.href);
+                url.searchParams.set('query', encodeURIComponent(valorAtual));
+                window.location.href = url.toString();
+            }
+        });
+
+        function showDelete(id) {
+            window.location.href = '?id=' + id + '&action=delete';
+        }
+
+        function SearchByTag(url_tag) {
+            window.location.href = '?' + url_tag + '=';
+        }
+
+
     </script>
 
-    <?php include "includes/footer.php" ?>
+    <?php require_once "includes/footer.php" ?>
 </body>
 
 </html>
